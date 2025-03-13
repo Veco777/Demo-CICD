c89df67a50be3eacd16d91d6117a3e704df6090c
Veco777 <CALEBTANG.24@ichat.sp.edu.sg>
2025-03-13 19:17:37 +0800

    Change

---
+++ Main.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.lang.Character;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+
+
+public class Main {
+
+    
+
+    public static void main(String[] args) {
+        
+        /* Exercise 1 = Pyramid Number pattern */
+        /* for (int x = 1; x <= 4; x++) {
+            
+            int w = 1 + (x -1)*2;
+            for (int y =0; y < 5-x; y++) {
+                System.out.print(" ");
+            } 
+            for (int y =0; y < w; y++) {
+                System.out.print("*");
+            } 
+            for (int y =0; y < 5-x; y++) {
+                System.out.print(" ");
+            } 
+            System.out.println();
+
+        }; */
+        
+        /* Exercise 2 = Square */
+        /* int length = 6; int height = 12;
+        for (int i =0; i < height;i++) {
+            if (i == 0 || height - i == 1) {
+                for (int j = 0; j < length; j++) {
+                    System.out.print("* ");
+                }
+            } 
+            else {
+                for (int k = 0; k< length;k++) {
+                    if (k == 0 || length - k == 1) {
+                        System.out.print("* ");
+                    }
+                    else System.out.print("  ");
+                }
+            }
+            System.out.println();
+        } */
+
+        /* Exercise 3 = Number Pattern */
+
+    };
+
+
+
+
+}
---
+++ item.java
@@ -0,0 +1,22 @@
+public class item {
+    String color = "Red";
+    double size = 39.0; 
+    boolean rented = false; 
+    
+        
+
+
+    void drive() {
+        System.out.println("You drive the car");
+    };
+
+    void stop() {
+        System.out.println("You stopped the car");
+    };
+
+    void honk() {
+        System.out.println("you Honked the car ");
+    }
+    
+
+}
